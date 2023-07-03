d0f1912836e0a5dc18b63b143f99e32693d523c9
Prajapati omkar <72141037+prajapatiomkar@users.noreply.github.com>
2023-07-03 12:43:08 +0530

    Fixed Some Error while connecting to frontend

--- controller/Auth.js
+++ controller/Auth.js
@@ -17,12 +17,20 @@ exports.createUser = async (req, res) => {
         const user = new User({ ...req.body, password: hashedPassword, salt });
         const doc = await user.save();
 
-        req.login(sanitizeUser(doc), (err) => {  // this also calls serializer and adds to session
+        req.login(sanitizeUser(doc), (err) => {
+          // this also calls serializer and adds to session
           if (err) {
             res.status(400).json(err);
           } else {
             const token = jwt.sign(sanitizeUser(doc), SECRET_KEY);
-            res.status(201).json(token);
+            console.log({ "token":token });
+            res
+              .cookie('jwt', token, {
+                expires: new Date(Date.now() + 3600000),
+                httpOnly: true,
+              })
+              .status(201)
+              .json({id:doc.id, role:doc.role});
           }
         });
       }
@@ -33,9 +41,19 @@ exports.createUser = async (req, res) => {
 };
 
 exports.loginUser = async (req, res) => {
-  res.json(req.user);
+  res
+    .cookie('jwt', req.user.token, {
+      expires: new Date(Date.now() + 3600000),
+      httpOnly: true,
+    })
+    .status(201)
+    .json(req.user.token);
 };
 
-exports.checkUser = async (req, res) => {
-  res.json({status:'success',user: req.user});
+exports.checkAuth = async (req, res) => {
+  if(req.user){
+    res.json(req.user);
+  } else{
+    res.sendStatus(401);
+  }
 };
--- services/common.js
+++ services/common.js
@@ -1,9 +1,21 @@
-const passport = require('passport');
+const passport = require("passport");
 
 exports.isAuth = (req, res, done) => {
-  return passport.authenticate('jwt')
+  return passport.authenticate("jwt");
 };
 
-exports.sanitizeUser = (user)=>{
-    return {id:user.id, role:user.role}
-}
+exports.sanitizeUser = (user) => {
+  return { id: user.id, role: user.role };
+};
+
+exports.cookieExtractor = function (req) {
+  let token = null;
+  if (req && req.cookies) {
+    token = req.cookies["jwt"];
+  }
+
+  //for testing purposes puting token here
+  token =  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0YTI3MWQ1MDQ5YzFmYzIwZjQ4MGQ4ZSIsInJvbGUiOiJ1c2VyIiwiaWF0IjoxNjg4MzY3NTczfQ.9fTOB5HAMjNVejlIU3_BPpNGsTSdx2r1kUSTnBeCzrw";
+
+  return token;
+};
