258c3a45420403b79bfa5d5b1d8673421084173e
Prajapati omkar <72141037+prajapatiomkar@users.noreply.github.com>
2023-07-04 12:38:56 +0530

    Test Deploy

--- index.js
+++ index.js
@@ -95,6 +95,11 @@ server.use("/auth", authRouter.router);
 server.use("/cart", isAuth(), cartRouter.router);
 server.use("/orders", isAuth(), ordersRouter.router);
 
+// this line we add to make react router work in case of other routes doesnt match
+server.get("*", (req, res) =>
+  res.sendFile(path.resolve("build", "index.html"))
+);
+
 // Passport Strategies
 passport.use(
   "local",
@@ -173,7 +178,7 @@ passport.deserializeUser(function (user, cb) {
 const stripe = require("stripe")(process.env.STRIPE_SERVER_KEY);
 
 server.post("/create-payment-intent", async (req, res) => {
-  const { totalAmount } = req.body;
+  const { totalAmount,orderId } = req.body;
 
   // Create a PaymentIntent with the order amount and currency
   const paymentIntent = await stripe.paymentIntents.create({
@@ -181,7 +186,11 @@ server.post("/create-payment-intent", async (req, res) => {
     currency: "inr",
     automatic_payment_methods: {
       enabled: true,
-    },
+    },metadata:{
+      orderId 
+      // this info will go to stripe => and then to our webhook
+      // so we can conclude that payment was successful, even if client closes window after pay
+    }
   });
 
   res.send({
